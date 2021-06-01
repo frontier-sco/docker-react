b9b96f4cdd238053bb9879a0ac3f5c8e07277f17
Paul Scott <pausba@gmail.com>
2021-06-01 15:51:53 +0000

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hell from Hell!
+          Hell from Hell! Feature Brancz
         </p>
         <a
           className="App-link"
